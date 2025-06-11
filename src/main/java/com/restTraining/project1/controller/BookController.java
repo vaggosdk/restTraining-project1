f0e264dbf7e4381408b083220a00762c3d04e32e
DESKTOP-PQ8J3FF\vaggo <vaggosdk@gmail.com>
2025-06-11 19:40:46 +0300

    Create book api, with dummy books

--- src/main/java/com/restTraining/project1/controller/BookController.java
+++ src/main/java/com/restTraining/project1/controller/BookController.java
@@ -1,13 +1,34 @@
 package com.restTraining.project1.controller;
 
+import com.restTraining.project1.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
 
-    @GetMapping("/api-endpoint")
-    public String firstApi() {
-        return "Hello, this is a response from the API endpoint!";
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController() {
+        initializeBooks();
+    }
+
+    private void initializeBooks() {
+        books.addAll(List.of(
+                new Book("Tiitle one", "Author one", "science"),
+                new Book("Tiitle two", "Author two", "science"),
+                new Book("Tiitle three", "Author three", "history"),
+                new Book("Tiitle four", "Author four", "math"),
+                new Book("Tiitle five", "Author five", "math"),
+                new Book("Tiitle six", "Author six", "math")
+        ));
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks() {
+        return books;
     }
 }
