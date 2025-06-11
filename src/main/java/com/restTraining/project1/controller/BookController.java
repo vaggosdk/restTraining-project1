ad397c1a544430d0ebf0d1d11e7058f52ab4fb09
DESKTOP-PQ8J3FF\vaggo <vaggosdk@gmail.com>
2025-06-11 23:14:42 +0300

    Add PUT endpoint using title

--- src/main/java/com/restTraining/project1/controller/BookController.java
+++ src/main/java/com/restTraining/project1/controller/BookController.java
@@ -54,4 +54,14 @@ public class BookController {
             books.add(newBook);
         }
     }
+
+    @PutMapping("/api/books/{title}")
+    public void updateBook(@PathVariable String title, @RequestBody Book updatedBook) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getTitle().equalsIgnoreCase(title)) {
+                books.set(i, updatedBook);
+                return;
+            }
+        }
+    }
 }
